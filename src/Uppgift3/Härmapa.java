3bba6d9483e7e12b92f2267cf17195d700dbe837
joasam-nac <joakim.samuelsson@yh.nackademin.se>
2025-10-03 10:52:05 +0200

    Uppgift 3

---
+++ src/Uppgift3/Härmapa.java
@@ -0,0 +1,12 @@
+package Uppgift3;
+
+import java.util.Scanner;
+
+public class Härmapa {
+    static void main() {
+        Scanner läsIn = new Scanner(System.in);
+
+        String text = läsIn.nextLine();
+        System.out.println("Du skrev: " + text);
+    }
+}
